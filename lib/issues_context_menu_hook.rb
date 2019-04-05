e66f0d6bcfcd8ea24a698611f0057ef94c00e869
bilel-kedidi <bilel.kedidi@gmail.com>
2019-04-05 10:06:16 +0100

    fix

--- lib/issues_context_menu_hook.rb
+++ lib/issues_context_menu_hook.rb
@@ -1,7 +1,7 @@
 class IssuesContextMenuHook < Redmine::Hook::ViewListener
   include ActionView::Helpers::TagHelper
 
-  render_on :view_issues_context_menu_start,
-            :partial => 'context_menus/issue_projects'
+  # render_on :view_issues_context_menu_start,:partial => 'context_menus/issue_projects'
+  render_on :view_issues_context_menu_start,:partial => 'context_menus/issues_parents'
 
 end
